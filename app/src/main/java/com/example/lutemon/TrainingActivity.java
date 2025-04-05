31a215b2550de5d13f859ad60ff400a17917ce9f
davidfrmn <furmandavid@proton.me>
2025-04-06 02:48:04 +0300

    Activities

--- app/src/main/java/com/example/lutemon/TrainingActivity.java
+++ app/src/main/java/com/example/lutemon/TrainingActivity.java
@@ -10,47 +10,23 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class MainActivity extends AppCompatActivity {
+public class TrainingActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_training);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
-
-
     }
 
-    public void launchLutemonSelector(View view) {
-        Intent intent = new Intent(this, SelectLutemonActivity.class);
+    public void launchMenu(View view) {
+        Intent intent = new Intent(this, MainActivity.class);
         startActivity(intent);
     }
-
-    public void launchTraining(View view) {
-        Intent intent = new Intent(this, TrainingActivity.class);
-        startActivity(intent);
-    }
-
-    public void launchBattle(View view) {
-        Intent intent = new Intent(this, BattleActivity.class);
-        startActivity(intent);
-    }
-
-    public void launchStatistics(View view) {
-        Intent intent = new Intent(this, StatisticsActivity.class);
-        startActivity(intent);
-    }
-
-    public void launchLutemonCreator(View view) {
-        Intent intent = new Intent(this, CreateLutemonActivity.class);
-        startActivity(intent);
-    }
-
-
 
 }
