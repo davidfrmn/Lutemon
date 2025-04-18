56ab9a5603f1c115a223fb6d82416206f8b03939
davidfrmn <furmandavid@proton.me>
2025-04-18 16:46:05 +0300

    added FileHandler set functions in Lutemon
    #TODO:
    -add lutemon.json
    -use the Filehandler
       - on lutemon creation
       - after battles <- xp and counters changed
       - after training <- xp and counters changed

--- app/src/main/java/com/example/lutemon/FileHandler.java
+++ app/src/main/java/com/example/lutemon/FileHandler.java
@@ -1,3 +1,5 @@
+package com.example.lutemon;
+
 import android.content.Context;
 
 import com.example.lutemon.lutemons.BlackLutemon;
@@ -13,7 +15,6 @@ import org.json.JSONObject;
 
 import java.io.BufferedReader;
 import java.io.FileWriter;
-import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
